9bdae397a9a547f98b704c20129cee41b36425d1
unknown <ferrelaxel@gmail.com>
2024-04-12 10:23:43 -0600

    Funcionalidad agregada

--- Ejercicio_10/Server.js
+++ Ejercicio_10/Server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -6,41 +7,36 @@ app.get('/convert', (req, res) => {
    
 
     const fs = require('fs');
-const jsdom = require('jsdom');
-const csvWriter = require('csv-writer').createObjectCsvWriter;
+    const jsdom = require('jsdom');
+    const csvWriter = require('csv-writer').createObjectCsvWriter;
+    const html = fs.readFileSync('Main.html', 'utf-8');
+    const dom = new jsdom.JSDOM(html);
+    const document = dom.window.document;
+    const countries = Array.from(document.querySelectorAll('.country'));
 
-const html = fs.readFileSync('Main.html', 'utf-8');
-const dom = new jsdom.JSDOM(html);
-const document = dom.window.document;
-
-const countries = Array.from(document.querySelectorAll('.country'));
-
-const data = countries.map(country => {
+    const data = countries.map(country => {
     const name = country.querySelector('.country-name').textContent.trim();
     const capital = country.querySelector('.country-capital').textContent.trim();
     const population = country.querySelector('.country-population').textContent.trim();
     const area = country.querySelector('.country-area').textContent.trim();
 
     return {name, capital, population, area};
-});
+    });
 
-const writer = csvWriter({
-    path: 'countries.csv',
-    header: [
-        {id: 'name', title: 'Country'},
+    const writer = csvWriter({
+        path: 'Paises.csv',
+        header: [
+        {id: 'name', title: 'Pais'},
         {id: 'capital', title: 'Capital'},
-        {id: 'population', title: 'Population'},
-        {id: 'area', title: 'Area'}
-    ]
-});
+        {id: 'population', title: 'Poblacion'},
+        {id: 'area', title: 'Area que ocupa en km2'}
+        ]
+    });
 
-writer.writeRecords(data).then(() => console.log('CSV file written successfully'));
-
-
-
-    res.send('CSV file written successfully');
-});
+    writer.writeRecords(data).then(() => console.log('CSV creado exitosamente'));
+    res.send('CSV creado exitosamente');
+    });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Servidor ejecutandose http://localhost:${port}`);
 });
