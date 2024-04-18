63cd5197605d17a9c5c3c7198ebd06b9ba7cb8cc
unknown <ferrelaxel@gmail.com>
2024-04-18 10:39:26 -0600

    Graficas

--- Ejercicio_11/practica11.js
+++ Ejercicio_11/practica11.js
@@ -41,23 +41,92 @@ function mostrarDatos(datos) {
     });
 }
 
-// Función para mostrar una gráfica de barras
-function mostrarGrafica(datos) {
-    // Obtener los valores de duración y fechas
-    const duraciones = datos.map(row => row['Duracion(Segundos)']);
-    const fechas = datos.map(row => row['Fecha en que ocurrio']);
+// Función para mostrar una gráfica de barras de poblacion
+function mostrarGrafica1(datos) {
+    // Filtrar los datos para descartar los que tienen valor 0 en 'Poblacion'
+    var datosFiltrados = datos.filter(row => row['Poblacion'] != 0);
+
+    // Obtener los valores de Capital y Poblacion
+    const Pobla = datosFiltrados.map(row => row['Poblacion']);
+    const Capitales = datosFiltrados.map(row => row['Capital']);
 
     // Crear el contexto del gráfico
-    var ctx = document.getElementById('graficaDuraciones').getContext('2d');
+    var ctx = document.getElementById('graficaPaises').getContext('2d');
 
     // Crear la gráfica de barras
     var grafica = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: fechas,
+            labels: Capitales,
             datasets: [{
-                label: 'Duración de avistamientos (segundos)',
-                data: duraciones,
+                label: 'Cantidad de poblacon por capital de pais',
+                data: Pobla,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 2
+            }]
+        },
+        options: {scales: {yAxes: [{ticks: {beginAtZero: true}}]}}
+    });
+}
+
+// Función para mostrar una gráfica de barras de area
+function mostrarGrafica2(datos) {
+    // Filtrar los datos para descartar los que tienen valor 0 en 'Area'
+    var datosFiltrados = datos.filter(row => row['Area que ocupa en km2'] <= 500000);
+
+    // Obtener los valores de Capital y Poblacion
+    const Area = datosFiltrados.map(row => row['Area que ocupa en km2']);
+    const Capitales = datosFiltrados.map(row => row['Capital']);
+
+    // Crear el contexto del gráfico
+    var ctx = document.getElementById('graficaArea').getContext('2d');
+
+    // Crear la gráfica de barras
+    var graficaAr = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: Capitales,
+            datasets: [{
+                label: 'Area que ocupa la capital en km2',
+                data: Area,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 2
+            }]
+        },
+        options: {
+            indexAxis: 'y',  // Cambiar el eje de índice a 'y' para hacer la gráfica horizontal
+            scales: {
+                x: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+
+// Función para mostrar una gráfica de líneas
+function mostrarGrafica3(datos) {
+    // Filtrar los datos para descartar los que tienen valor 0 en 'Poblacion'
+    var datosFiltrados = datos.filter(row => row['Poblacion'] <= 500000 && row['Poblacion'] != 0);
+
+    // Obtener los valores de Capital y Poblacion
+    const Pobla = datosFiltrados.map(row => row['Poblacion']);
+    const Capitales = datosFiltrados.map(row => row['Capital']);
+
+    // Crear el contexto del gráfico
+    var ctx = document.getElementById('graficaLineas').getContext('2d');
+
+    // Crear la gráfica de líneas
+    var grafica = new Chart(ctx, {
+        type: 'line',  // Cambiar 'bar' a 'line'
+        data: {
+            labels: Capitales,
+            datasets: [{
+                label: 'Cantidad de poblacion por capital de pais',
+                data: Pobla,
                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 borderColor: 'rgba(54, 162, 235, 1)',
                 borderWidth: 3
@@ -65,21 +134,30 @@ function mostrarGrafica(datos) {
         },
         options: {
             scales: {
-                yAxes: [{
+                x: {
+                    ticks: {
+                        autoSkip: false,  // Desactivar el salto automático de etiquetas
+                        maxRotation: 90,  // Permitir una rotación máxima de 90 grados
+                        minRotation: 90   // Establecer una rotación mínima de 90 grados
+                    }
+                },
+                y: {
                     ticks: {
                         beginAtZero: true
                     }
-                }]
+                }
             }
         }
     });
 }
 
 
-
 // Llamar a la función para cargar los datos y luego mostrar la tabla
 cargarDatos().then(function(data) {
     mostrarDatos(data); // Mostrar la tabla
+    mostrarGrafica1(data); // Mostrar la gráfica
+    mostrarGrafica2(data); // Mostrar la gráfica
+    mostrarGrafica3(data); // Mostrar la gráfica
 }).catch(function(error) {
     console.error('Error al cargar los datos:', error);
 });
